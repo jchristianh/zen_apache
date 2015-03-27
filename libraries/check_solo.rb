c0d15f8847d92bac15f1772f606ecc2e6b01b5fd
Chris Hammer <chris@thezengarden.net>
2015-03-27 09:38:43 -0400

    Testing one more way of doing a fail

--- libraries/check_solo.rb
+++ libraries/check_solo.rb
@@ -22,7 +22,7 @@ class Chef::Recipe::CheckSolo
   def self.check_solo?
     #if Chef::Config['solo']
       #Chef::Application.fatal!("This recipe requires backing by a Chef server.")
-      Chef::Log.fatal("This recipe requires backing by a Chef server.")
+      Chef::Log.error("This recipe requires backing by a Chef server.")
       exit 1
     #end
   end
