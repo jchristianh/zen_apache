d2516f1b189ce1e8fa120c3ab5cd0b98a899ca9b
Chris Hammer <chris@thezengarden.net>
2017-05-10 08:33:32 -0400

    1.3.9-DEV-170510-834 -----------------------------         * MODIFY:           - directory logic for vhosts modified slightly; now             chef-client 13 compatible

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'chris@thezengarden.net'
 license          'GPL v2'
 description      'Installs/Configures zen_apache'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.3.8'
+version          '1.3.9'
 issues_url       'http://www.thezengarden.net'
 source_url       'http://www.thezengarden.net'
