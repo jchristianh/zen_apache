3644dc2c5c37b0b5e8db3fdf4e3c469325ae6d6e
Chris Hammer <chris@thezengarden.net>
2018-09-20 13:28:33 -0400

    1.3.20-DEV-190920-1330 -----------------------------         * ADD:           - add support for redirecting requests (mostly for redirect             non-ssl to ssl requests)

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'chris@thezengarden.net'
 license          'GPL v2'
 description      'Installs/Configures zen_apache'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.3.10'
+version          '1.3.20'
 issues_url       'http://www.thezengarden.net'
 source_url       'http://www.thezengarden.net'
