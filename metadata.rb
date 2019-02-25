1b7d2aac75bad5f4208c58a20dca2d1ad9d5916a
Chris Hammer <chris@thezengarden.net>
2019-02-25 07:32:02 -0500

    1.3.20-DEV-190225-73126 -----------------------------     * MODIFY:       - enabling php 7.3 and disabling php 7.0; block for         php 7.0 disable will be removed in a future release
          - updated some foodcritic rules; build may still break
            until i have time to re-tool a bit

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name             'zen_apache'
-maintainer       'Copyright (C) 2015 Chris Hammer'
+maintainer       'Copyright (C) 2019 Chris Hammer'
 maintainer_email 'chris@thezengarden.net'
 license          'GPL v2'
 description      'Installs/Configures zen_apache'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.3.20'
+version          '1.3.30'
 issues_url       'http://www.thezengarden.net'
 source_url       'http://www.thezengarden.net'
--- recipes/server.rb
+++ recipes/server.rb
@@ -42,8 +42,13 @@ if node['apache_conf']['php']['use_ver_7']
 
 
   execute 'enabling-remi-repos' do
-    command 'yum-config-manager --enable remi remi-php70'
-    not_if 'grep enabled=1 /etc/yum.repos.d/remi-php70.repo'
+    command 'yum-config-manager --enable remi remi-php73'
+    not_if 'grep enabled=1 /etc/yum.repos.d/remi-php73.repo'
+  end
+
+  execute 'disabling-remi-repo-70' do
+    command 'yum-config-manager --disable remi-php70'
+    not_if 'grep enabled=0 /etc/yum.repos.d/remi-php70.repo'
   end
 end
 
