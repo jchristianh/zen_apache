8805306740402bed2af3724688b78bfd98d56915
Chris Hammer <chris@thezengarden.net>
2019-03-11 08:26:56 -0400

    1.3.38-DEV-190311-82612 -----------------------------     * MODIFY:       - versions after 7.0 don't seem to be working out;         killing off 7.2 and setting primary back to 7.0

--- recipes/server.rb
+++ recipes/server.rb
@@ -42,13 +42,13 @@ if node['apache_conf']['php']['use_ver_7']
 
 
   execute 'enabling-remi-repos' do
-    command 'yum-config-manager --enable remi remi-php72'
-    not_if 'grep enabled=1 /etc/yum.repos.d/remi-php72.repo'
+    command 'yum-config-manager --enable remi remi-php70'
+    not_if 'grep enabled=1 /etc/yum.repos.d/remi-php70.repo'
   end
 
-  execute 'disabling-remi-repo-73' do
-    command 'yum-config-manager --disable remi-php73'
-    only_if 'grep enabled=1 /etc/yum.repos.d/remi-php73.repo'
+  execute 'disabling-remi-repo-72' do
+    command 'yum-config-manager --disable remi-php72'
+    only_if 'grep enabled=1 /etc/yum.repos.d/remi-php70.repo'
   end
 end
 
